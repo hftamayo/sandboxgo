eee587d4e2ef7044c367916aedcf1ad2ba1a1cf9
hftamayo <hftamayo@gmail.com>
2022-12-08 15:44:53 -0600

    muxRouter 2nd ver: query params

--- restfulwservices/02routing/src/muxRouter.go
+++ restfulwservices/02routing/src/muxRouter.go
@@ -14,11 +14,20 @@ func ArticleHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Category is: %v\n", vars["category"])
 	fmt.Fprintf(w, "ID is: %v\n", vars["id"])
 }
+//Query-based matching
+func QueryHandler(w http.ResponseWriter, r *http.Request) {
+	queryParams := r.URL.Query()
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Got parameter id:%s!\n", queryParams["id"][0])
+	fmt.Fprintf(w, "Got parameter category:%s!", queryParams["category"][0])
+}
+
 func main() {
 	//create a new router
 	r := mux.NewRouter()
 	//attach a path with handler
-	r.HandleFunc("/articles/{category}/{id:[0-9]+}", ArticleHandler)
+	r.HandleFunc("/articles", QueryHandler)
+	r.Queries("id", "category")
 	srv := &http.Server{
 		Handler: r,
 		Addr: "127.0.0.1:8000",
@@ -26,5 +35,6 @@ func main() {
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout: 15 * time.Second,
 	}
+
 	log.Fatal(srv.ListenAndServe())
 }
