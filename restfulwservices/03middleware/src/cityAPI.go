e8e7c5a0dd6deaca029f9fe4613e40a5660b6466
hftamayo <hftamayo@gmail.com>
2022-12-19 14:44:12 -0600

    cityAPI: a few changes

--- restfulwservices/03middleware/src/cityAPI.go
+++ restfulwservices/03middleware/src/cityAPI.go
@@ -24,11 +24,14 @@ func mainLogic(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Got %s city with area of %d sq miles!\n", tempCity.Name, tempCity.Area)
 		//confirm everything is working as expected
 		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("201 - Created"))
+	}else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
 		w.Write([]byte("405 - Method Not Allowed"))
 	}
 }
 
 func main(){
 	http.HandleFunc("/city", mainLogic)
-	http.ListenAndServer(":8000", nil)
+	http.ListenAndServe(":8000", nil)
 }
